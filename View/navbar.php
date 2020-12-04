916fe98145423a0a119c345ffd53a71f7fea984c
clherrington <colin.herrington330@gmail.com>
2020-12-04 10:16:59 -0500

    Hello

--- View/navbar.php
+++ View/navbar.php
@@ -3,7 +3,7 @@
     <img class= 'logo-img' src='./View/Public/Photos/Path 1.png'>
     <h3 class= 'h3-nav'>About Us</h3>
     <h3 class= 'h3-nav'>Merch</h3>
-    <h2 class= 'h2-nav button'>GFY Pepper Company</h2>
+    <a  onclick='href="../GFY-Pepper-Company/Index.php"' class= 'no-line-link h2 h2-nav'>GFY Pepper Company</a>
     <h3 class= 'h3-nav'>Hot Sauce</h3>
     <h3 class= 'h3-nav btn'>Food Stuffs</h3>
     <img class= 'shopping-cart-img' src='./View/Public/Photos/blacksc.png'>
